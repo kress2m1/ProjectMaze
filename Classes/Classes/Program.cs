d8500e06c3ffa45724813d619d1d49a1abd3634d
Andre Dada <kress2m1@gmail.com>
2016-05-16 14:56:47 +0100

    Stashed some files

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -72,6 +72,11 @@ namespace Classes
             viewArrays.ArraySamples();
             Console.WriteLine(".............");
 
+            //Call the Variable class here
+            varMethod.varString("Andre", "Dada");
+            Console.WriteLine(".............");
+
+
 
             Console.ReadKey();
         }
@@ -80,6 +85,7 @@ namespace Classes
         public static void externalMethodInMain()
         {
             Console.WriteLine("This is a method called in the Main method");
+            Console.WriteLine(".............");
         }       
     }
 }
